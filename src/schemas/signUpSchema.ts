11f446b7f2d11ac53f20958ff7b97ac0c196ec6c
Manish7113 <kumawatji7113@gmail.com>
2024-08-25 14:10:18 +0530

    updates

--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -5,6 +5,5 @@ import {z} from 'zod'
 export const signInschema = z.object({
     userName : z.string().min(3,'User Name Should More Than 3 Characters'),
     password : z.string().min(6, {message : 'Password Length Should Be More Than 6 Characters'}),
-    confirmPassword : z.string().min(6, {message : 'Password Length Should Be More Than 6 Characters'}),
     mobileNumber : z.string().min(10, {message:'Enter Valid Number'}).max(11,{message: 'Enter Valid Number'}) 
 })
